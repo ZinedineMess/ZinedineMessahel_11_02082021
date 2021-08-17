3b78d9093d9ff135d4f8d316f4c23bda36a8d278
ZinedineMess <zinedinemessahel@hotmail.com>
2021-08-17 12:46:28 +0200

    refactor: changement class App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import '../../index.css';
 import {data} from '../../datas/data';
@@ -9,29 +9,31 @@ import AboutPage from '../../views/AboutPage/AboutPage';
 import ApartmentPage from "../../views/ApartmentPage/ApartmentPage";
 import Error404 from "../Error404/Error404";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path='/'>
-              <HomePage />
-          </Route>
-          <Route exact path="/apartment/:id" render={(props) => (
-              <ApartmentPage {...props} apartments={data} />
-              )}/>
-          <Route exact path='/About'>
-            <AboutPage />
-          </Route>
-          <Route>
-            <Error404 />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path='/'>
+                <HomePage />
+            </Route>
+            <Route exact path="/apartment/:id" render={(props) => (
+                <ApartmentPage {...props} apartments={data} />
+                )}/>
+            <Route exact path='/About'>
+              <AboutPage />
+            </Route>
+            <Route>
+              <Error404 />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
