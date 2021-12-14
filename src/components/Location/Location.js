7bf4a50c8151a89f2198ad721e54a2bc4b9bf100
ZinedineMess <zinedinemessahel@hotmail.com>
2021-12-14 17:38:51 +0100

    feat: init static page

--- src/components/Location/Location.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class Location extends Component {
-    render() {
-        return <h3 className="apartmentLocation">{this.props.location}</h3>
-    }
-}
-
-export default Location;
--- src/components/Tag/Tag.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class Tag extends Component {
-    render() {
-        return <span className="apartmentTag">{this.props.tag}</span>
-    }
-}
-
-export default Tag;
